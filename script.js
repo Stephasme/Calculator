980af600181c44393bd42a432669e549544db8b8
Stéphane Lorio <lorio.stephane@gmail.com>
2023-02-11 17:41:06 +0100

    changed file

--- script.js
+++ script.js
@@ -1,13 +1,100 @@
-function clearScreen() {
-    document.getElementById("result").value = "";
-  }
+function add(a,b){
+  return (a + b);
+}
 
-  function display(value) {
-    document.getElementById("result").value += value;
-  }
+function substract(a,b){
+  return (a - b);
+}
 
-  function calculate() {
-    let p = document.getElementById("result").value;
-    let q = eval(p);
-    document.getElementById("result").value = q;
+function multiply(a,b){
+  return (a * b);
+}
+
+function divide(a,b){
+  if (b === 0) {
+    alert("Nope!! Can't do that");
   }
+  else{
+    return (a/b);
+  }
+}
+
+function operate(operator, a, b){
+  switch (operator){
+    case '+':
+      result = add(a,b);
+      console.log(result);
+      break;
+    case '-':
+      result = substract(a,b);
+      console.log(result);
+      break;
+    case 'x':
+      result = multiply(a,b);
+      console.log(result);
+      break;
+    case '/':
+      result = divide(a,b);
+      console.log(result);
+  }
+  return result;
+}
+
+function calculate(){
+
+  let operator = '';
+  let input ='';
+  let a ='';
+  let b ='';
+
+  const buttons = document.querySelectorAll("button");
+  buttons.forEach((button)=>{
+    button.addEventListener('click',()=>{
+      let displayValue = button.value;
+      let screen = document.getElementById('screen');
+
+      if (displayValue === "+" ||displayValue === "-"
+          || displayValue === "x" ||displayValue === "/" ){
+            operator = displayValue;
+            a = input;
+            input = "";
+          }
+      else if (displayValue === "."){
+        if (input.includes(".")){
+        }
+        else{
+          input += displayValue;
+        }
+      }
+
+      else if (displayValue === ""){
+        input = input.slice(0,-1);
+        console.log(input);
+      }
+      else if (displayValue === "clear") {
+        operator = "";
+        input = "";
+        a = "";
+        b = "";
+        screen.textContent = "";
+      }
+
+      else if (displayValue === "="){
+        b = input;
+        if (a === "" || operator === "") {
+          alert('Not working');
+        } else {
+          input = operate(operator, parseFloat(a), parseFloat(b)).toFixed(2);
+          screen.textContent = input;
+        }
+      }
+      else {
+        input += displayValue;
+      }
+      screen.textContent = input;
+      console.log(input);
+    });
+  });
+}
+
+calculate();
